e5a221175ae43975345bff984fda406a588be652
mrJavad <seydi.birjand@gmail.com>
2021-11-07 10:51:41 +0330

    create panel base

--- app/Http/Livewire/User/Panel.php
+++ app/Http/Livewire/User/Panel.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\Payment;
 use Livewire\Component;
 
 class Panel extends Component
 {
+    public $account ;
+    public $payment ;
+    public function mount(){
+        $this->account = session('account');
+        $this->payment = Payment::where('account_id',$this->account['id'])->first();
+    }
     public function render()
     {
-        return view('livewire.user.panel');
+        return view('livewire.user.panel' ,
+        [
+            'account'=>$this->account,
+            'payment'=>$this->payment
+        ]
+        );
     }
 }
---
+++ database/migrations/2021_11_07_061643_create_gateways_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGatewaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gateways', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('account_id')->constrained('accounts')->onDelete('cascade');
+            $table->bigInteger('gateway_amount')->default(0);
+            $table->text('token');
+            $table->string('tracking_number');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gateways');
+    }
+}
