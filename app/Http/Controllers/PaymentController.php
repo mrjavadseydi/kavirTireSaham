3827a293ec57a7a579ef8bb4ca978740f5813635
mohammad javad seydi <seydi.birjand@gmail.com>
2021-12-07 10:06:48 +0330

    update payment controller

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -10,13 +10,9 @@ use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
-    public function payment(Request $request){
-        if ($request->responseCode != '00') {
-            return view('faild', [
-                'track' => $request->paymentId,
-                'token' => $request->token
-            ]);
-        } else {
+    public function payment(Request $request)
+    {
+        if ($request->responseCode == '00') {
             $gateway = Gateway::where([
                 [
                     'token', $request->token
@@ -31,12 +27,7 @@ class PaymentController extends Controller
                     'status', 0
                 ]
             ])->first();
-            if (!$gateway) {
-                return view('faild', [
-                    'track' => $request->paymentId,
-                    'token' => $request->token
-                ]);
-            } else {
+            if ($gateway) {
                 $gateway->update([
                     'systemTraceAuditNumber' => $request->systemTraceAuditNumber
                 ]);
@@ -56,27 +47,32 @@ class PaymentController extends Controller
                 $gateway->update([
                     'status' => 1,
                 ]);
-                if($gateway->type==0){
+                if ($gateway->type == 0) {
                     Payment::create([
                         'account_id' => $account->id,
                         'gateway_id' => $gateway->id,
                         'local_pay' => $account->current_stock * 1000,
                     ]);
-                }elseif($gateway->type == 1){
+                } elseif ($gateway->type == 1) {
                     SelfReport::create([
                         'account_id' => $account->id,
                         'gateway_id' => $gateway->id,
-                        'count' =>$gateway->gateway_amount/1000
+                        'count' => $gateway->gateway_amount / 1000
                     ]);
-                }elseif($gateway->type == 2){
-                    Payment::where('account_id',$account->id)->update([
-                        'gateway_id'=>$gateway->id
+                } elseif ($gateway->type == 2) {
+                    Payment::where('account_id', $account->id)->update([
+                        'gateway_id' => $gateway->id
                     ]);
                 }
-
                 session(['payment' => 'success']);
-                return redirect(\route('user.panel'));
+                return redirect(route('user.panel'));
             }
         }
+
+        return view('faild', [
+            'track' => $request->paymentId,
+            'token' => $request->token
+        ]);
+
     }
 }
